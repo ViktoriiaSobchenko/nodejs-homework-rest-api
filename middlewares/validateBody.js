5578f18c9d5eeed880c597896584a1972e63566b
ViktoriiaSobchenko <v.sobchenko11@gmail.com>
2023-06-18 20:15:13 +0300

    add mongodb

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,7 +2,8 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-
+    const { favorite } = req.body;
+      if (favorite === undefined) {next(HttpError(400, "missing field favorite"))}
     if (Object.keys(req.body).length === 0) {
       next(HttpError(400, "missing fields"));
   }
