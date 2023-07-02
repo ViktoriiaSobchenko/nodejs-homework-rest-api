bbc4df7fb18a3f6c35d1519cd1abeecf8b59ba8a
ViktoriiaSobchenko <v.sobchenko11@gmail.com>
2023-07-02 20:24:41 +0300

    add avatar

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,5 +3,13 @@ const isValidId = require("./isValidId");
 const isValidFavorite = require("./isValidFavorite");
 const validateUserBody = require("./validateUserBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = { validateBody, isValidId, isValidFavorite, validateUserBody, authenticate };
+module.exports = { 
+    validateBody,
+    isValidId,
+    isValidFavorite,
+    validateUserBody,
+    authenticate,
+    upload,
+};
